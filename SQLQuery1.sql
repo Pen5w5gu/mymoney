2934b2af8a8891dc8854359b056f8375385bdf36
Pen5w5gu <quannthe171942@fpt.edu.vn>
2024-01-15 21:17:24 +0700

    update database

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+﻿-- Tạo bảng người dùng
+CREATE TABLE dbo.Users (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Name VARCHAR(100),
+    Email VARCHAR(100) UNIQUE
+);
+
+-- Tạo bảng thu nhập
+CREATE TABLE dbo.ThuNhap (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT,
+    Ngay DATE,
+    SoTien DECIMAL(10, 2),
+    MoTa TEXT,
+    FOREIGN KEY (UserID) REFERENCES dbo.Users(ID)
+);
+
+-- Tạo bảng chi tiêu
+CREATE TABLE dbo.ChiTieu (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT,
+    Ngay DATE,
+    SoTien DECIMAL(10, 2),
+    MoTa TEXT,
+    FOREIGN KEY (UserID) REFERENCES dbo.Users(ID)
+);
